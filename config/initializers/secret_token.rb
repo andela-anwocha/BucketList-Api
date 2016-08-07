937eb1d964b091b32fd24a995705152bfdff28ee
andela-anwocha <adimoranma.felix@andela.com>
2016-08-07 22:11:13 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+BucketlistApi::Application.config.secret_token = '76377f2534ac7225c5dec83895e7fe64861670cffb56450240aaeba887632678655276f03191a4c07798cdfa83ac379c2aca887174672be67e388e228626434e'
